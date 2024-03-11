ef6884c98c76343181fc1d19c72d6751a8734ff5
Izahan <108328777+izahan-sodha@users.noreply.github.com>
2024-03-11 08:23:15 +0530

    Add files via upload

---
+++ pp5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int a,b,c;
+
+    cout << "enter a num" << endl;
+    cin >>a;
+    b=a*a;
+    c=a*a*a;
+    cout << "square of  num is :" <<b <<endl;
+    cout << "cube of  num is :" <<c << endl;
+    return 0;
+}
