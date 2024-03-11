ed9b2f5001da439996e87f615b65cc14b1d388d6
Izahan <108328777+izahan-sodha@users.noreply.github.com>
2024-03-11 08:24:25 +0530

    Add files via upload

---
+++ pp10.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "a" << endl;
+    cout << "a a" << endl;
+    cout << "a a a" << endl;
+   cout << "a a a a" << endl;   
+    return 0;
+}
