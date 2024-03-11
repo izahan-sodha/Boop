ac4f5d16c42b18ad161361f722f9e6592c5b9a86
Izahan <108328777+izahan-sodha@users.noreply.github.com>
2024-03-11 08:23:45 +0530

    Add files via upload

---
+++ pp6.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int a,b,c,d,e;
+
+    cout << "enter a three num" << endl;
+    cin >>a>>b>>c;
+    d=a+b+c;
+    e=d/3;
+    cout << "average of three of :" <<e<<endl;
+    return 0;
+}
